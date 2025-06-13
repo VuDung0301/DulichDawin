e01f46864bf9b620ee1640063e7d0868b5e19e0a
VuDung0301 <vu75296@gmail.com>
2025-06-14 01:33:35 +0700

    Remove unwanted files from repository

--- backend/src/middlewares/async.js
+++
@@ -1,9 +0,0 @@
-/**
- * Middleware bọc hàm async để xử lý lỗi
- * @param {Function} fn - Hàm async cần được bọc
- * @returns {Function} - Hàm middleware mới đã được bọc
- */
-const asyncHandler = (fn) => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next);
-
-module.exports = asyncHandler; 
